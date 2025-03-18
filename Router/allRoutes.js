155949b4d31e37c1cd578001b3d50c6bc7f4538f
aurelie <lieange1326@outlook.com>
2025-03-18 18:34:41 +0100

    add page-covoiturage-filtre

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/covoiturage","Covoiturage","/pages/covoiturage.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Eco Ride";
