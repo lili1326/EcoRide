6931e959a5df418f31c2b4d4c6b1539ff0d854ea
aurelie <lieange1326@outlook.com>
2025-03-25 14:57:01 +0100

    add.page-utilisateurs

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,10 @@ export const allRoutes = [
     new Route("/vueDetaileeCovoiturage","Vue détaillée du Covoiturage","/pages/vueDetaileeCovoiturage.html"),
     new Route("/signin"," Connection","/pages/auth/signin.html"),  
     new Route("/signinup"," Inscription","/pages/auth/signinup.html"), 
-    new Route("/account", "Mon espace", "/pages/auth/account.html", "/js/profil.js")
+    new Route("/account", "Mon espace", "/pages/auth/account.html", "/js/profil.js"),
+    new Route("/conducteur"," Conducteur","/pages/utilisateurs/conducteur.html"), 
+    new Route("/passager"," Passager","/pages/utilisateurs/passager.html"), 
+    new Route("/conducteurPassager","Conducteur et ou  Passager","/pages/utilisateurs/conducteurPassager.html"), 
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
