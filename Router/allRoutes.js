be37454a3ca47b6145d9556b241ee8951decedb3
aurelie <lieange1326@outlook.com>
2025-03-25 07:19:21 +0100

    add. signinup-inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/covoiturage","Covoiturage","/pages/covoiturage.html"),
     new Route("/vueDetaileeCovoiturage","Vue détaillée du Covoiturage","/pages/vueDetaileeCovoiturage.html"),
-    new Route("/signin"," Connection","/pages/signin.html"),  
-
+    new Route("/signin"," Connection","/pages/auth/signin.html"),  
+    new Route("/signinup"," Inscription","/pages/auth/signinup.html"),  
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
